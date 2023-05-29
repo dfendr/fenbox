4a8ad4e05f760eeda80f1e403e46245e587d19b3
D. Fender <26173007+postfen@users.noreply.github.com>
2023-05-28 22:36:44 -0600

    moved git rename to more seagreen color

--- lua/fenbox/theme.lua
+++ lua/fenbox/theme.lua
@@ -358,7 +358,7 @@ function M.setup(config)
     NvimTreeGitDirty = { fg = c.faded_yellow},
     NvimTreeGitStaged = { fg = c.soft_green},
     NvimTreeGitMerge = { fg = c.orange},
-    NvimTreeGitRenamed = { fg = c.forest_green},
+    NvimTreeGitRenamed = { fg = c.clean_green},
     NvimTreeGitNew = { fg = c.soft_green },
     NvimTreeGitDeleted = { fg = c.red },
 
